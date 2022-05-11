ce2a12a80cc44d5128c94b7d5e841c36b7ccb40e
Dillon Frawley <dfra0004@student.monash.edu>
2022-05-11 20:29:46 +1000

    added search results to map controller

--- FIT3178-Productive/MapViewController.swift
+++ FIT3178-Productive/MapViewController.swift
@@ -31,7 +31,19 @@ class MapViewController: UIViewController, UISearchResultsUpdating {
     }
     
     func updateSearchResults(for searchController: UISearchController) {
-        //
+        
+        guard let query = searchController.searchBar.text, !query .trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        GooglePlacesManager.shared.findPlaces(query: query) { result in
+            switch result {
+            case .success(let places):
+                print(places)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
     
